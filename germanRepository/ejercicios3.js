da7df5ea060bbb5564eadc822a12939f6a0764d8
augusto2501 <carlosaugustoaj@gmail.com>
2021-05-28 13:20:56 -0500

    actualización archivos

--- germanRepository/ejercicios3.js
+++ germanRepository/ejercicios3.js
@@ -13,16 +13,50 @@ function fifa18(params) {
 Cree un programa que declare e inicialice una constante que almacene el número pi y otra que almacena la URL de Google. Imprima cada una de las variables. Haga pseudocódigo y código.
 */
 
-function imprimirGoogle(params) {
+function googlePi(params="pi") {
 
     if(params==="pi"){
 
-    return Math.PI;
+        const numeroPi = Math.PI;
+
+    return numeroPi ;
 
     }
     if(params==="google"){
+
+        const google ="https://www.google.com/";
         
-        return "https://www.google.com/";
+        return google;
     }
+
+}
+
+/*
+Cree un programa que declare e inicialice una variable con el valor 50, e imprima tal variable cuatro veces.
+*/
+
+function imprimirNumero(){
+
+    let numero = 50;
+    let numero2  = 100;
+
+    return {
+        numero, numero2
+    }
+    
+
+}
+
+function mi_funcion(parametro1, parametro2){
+
+   //queremos que parametro1 tenga por defecto el valor 33
+
+   parametro1 = parametro1 || 33;
+
+   //queremos que parametro2 tenga por defecto el valor false
+
+   parametro2 = parametro2 || false;
+
+
 
 }
---
+++ germanRepository/seccion5.js
@@ -0,0 +1,53 @@
+/*
+1. Cree un programa que imprima los números enteros entre 0 y 100 en orden ascendente y descendente. Use ciclo for. Use funciones.
+*/
+
+function cicloFor(param) 
+{ 
+    let contadorDesc = param;
+    for (let index = 0; index <= param; index++) 
+    {
+        contadorDesc = param-index;
+        document.write (`${index}\t - \t${contadorDesc}<br>`);
+        
+    }
+ }
+
+/*
+2. Cree un programa que imprima la tabla de multiplicar del 3, del 0 al 50. Use ciclo for. Use funciones.
+*/
+
+function tabla3() {
+    
+    let multiplicacion =0;
+    for(let i = 1; i <=50;i++){
+        multiplicacion= 3*i;
+        console.log(`3 x ${i} = ${multiplicacion}`);
+    }
+    
+}
+
+/*
+3. Cree un programa que calcule el promedio de tres notas para n estudiantes. Use ciclo for. Use funciones.
+*/
+
+function promedio(){
+    
+    const estudiantes = parseInt(prompt("Digite el numero de estudiantes"));
+    let notas=[];
+    //let uno;
+
+    for(let i =0;i<estudiantes;i++){
+        nota = prompt("Digite las notas, separadas por una coma(,)");
+        notas=nota.split(",");
+        
+    }
+        console.log(typeof (parseFloat(notas[0])));
+        console.log(typeof (parseFloat(notas[1])));
+        console.log(typeof (parseFloat(notas[2])));
+
+}
+
+
+
+
