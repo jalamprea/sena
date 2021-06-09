8718bc9a62b5d03eb9ade9940a8953c41c836f4b
augusto2501 <carlosaugustoaj@gmail.com>
2021-06-09 16:18:37 -0500

    prueba vocales

---
+++ study/codewars/disemvowel_trolls.js
@@ -0,0 +1,9 @@
+function disemvowel(str) {
+  
+    //let value;
+    //console.log(str[3]);
+    for(let i = 0; i < str.length; i++) {
+      console.log(str[i].match(/[aeiou]/gi),i);
+    }
+  //return str;
+}
