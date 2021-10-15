ad39432d2382ebc683320d07680a7ed3fde5d1be
Julian Lamprea <jalamprea@gmail.com>
2021-10-15 18:08:15 -0500

    Validacion de turno y jugada actual

--- cognitus/chess-2/js/main.js
+++ cognitus/chess-2/js/main.js
@@ -29,42 +29,35 @@ function initial(n,m){
                 //Torre negra (B-R)
                 cell.innerHTML="&#9820;";
                 chess[i][j]='B-R';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===0 && ((j===1)||(j===6))){
                 //Caballo negro (B-N)
                 cell.innerHTML="&#9822;";
                 chess[i][j]='B-N';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===0 && ((j===2)||(j===5))){
                 //Alfil negro (B-B)
                 cell.innerHTML="&#9821;";
                 chess[i][j]='B-B';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===0 && j===3){
                 //Rey negro (B-K)
                 cell.innerHTML="&#9819;";
                 chess[i][j]='B-K';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===0 && j===4){
                 //Reina negra (B-Q)
                 cell.innerHTML="&#9818;";
                 chess[i][j]='B-Q';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===1){
                 //Peon negro (B-P)
                 cell.innerHTML="&#9823;";
                 chess[i][j]='B-P';
-                cell.setAttribute("id", `${i}-${j}`);
             }
 
             // if(i>=2 || i<=5)chess[i][j]='#';
               if(i>=2||i<=5 && chess[i][j]==''){
-                cell.setAttribute("id", `${i}-${j}`);
                 chess[i][j]='#'
             }
 
@@ -72,7 +65,6 @@ function initial(n,m){
                  //Peon blanco (W-P)
                 cell.innerHTML="&#9817;";
                 chess[i][j]='W-P';
-                cell.setAttribute("id", `${i}-${j}`);
             }
 
             if(i===7 && ((j===0)||(j===7))){
@@ -80,32 +72,32 @@ function initial(n,m){
                 cell.innerHTML="&#9814;";
                 // console.log(chess);
                 chess[i][j]='W-R';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===7 && ((j===1)||(j===6))){
                 //Caballo blanco (W-N)
                 cell.innerHTML="&#9816;";
                 chess[i][j]='W-N';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===7 && ((j===2)||(j===5))){
                 //Alfil blanco (W-B)
                 cell.innerHTML="&#9815;";
                 chess[i][j]='W-B';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===7 && j===3){
                 //Rey blanco (W-K)
                 cell.innerHTML="&#9813;";
                 chess[i][j]='W-K';
-                cell.setAttribute("id", `${i}-${j}`);
             }
             if(i===7 && j===4){
                 //Reina blanca (W-Q)
                 cell.innerHTML="&#9812;";
                 chess[i][j]='W-Q';
-                cell.setAttribute("id", `${i}-${j}`);
             }
+
+            // Atributos de cada celda
+            cell.setAttribute("id", `${i}-${j}`);
+            cell.dataset.fila = i
+            cell.dataset.col = j
            
             if(value%2!=0){    
                 cell.setAttribute("class", "democlass");
@@ -124,6 +116,8 @@ function initial(n,m){
     container.appendChild(table);
     console.log(chess);
 
+    table.addEventListener("click", captura_click, false);
+
 }
 
 function chessPieces(){
@@ -135,38 +129,127 @@ function chessPieces(){
 
 }
 
-    let ficha="";
-    let turno = "w";
+let fichaAnterior = null
+let turnoActual = "W";
 
-    function captura_click(e) {
-	
-        // console.log(e.path[0]);
-         console.log(e.path[0].id);
-        // ficha = e.
-        
-        // console.log(`Fila: ${e.path[1].rowIndex} Columna:${e.path[0].cellIndex} Ficha: ${e.path[0].innerHTML}`);
+function captura_click(e) {
     
-        if(e.path[0].innerHTML!="" ){
-            ficha = e.path[0].innerHTML;
-            e.path[0].innerHTML= "";
-            // console.clear();
-            console.log(e.path[0].id[0],e.path[0].id[2]);
+    const fichaDOM = e.path[0];
 
-            
-        }else if(e.path[0].innerHTML===""){
-            e.path[0].innerHTML = ficha;
-            ficha=""; 
-            // console.clear();
-            console.log(e.path[0].id[0],e.path[0].id[2]);
-            //  console.log(chess);
-        
-        }
-        
+    // Valida si es el turno correcto
+    if (!validarTurno(fichaDOM)) {
+        console.log('no es tu turno aun!')
+        return;
+    }
 
-           
+    // validar si di click sobre una ficha que ya estaba seleccionada
+    if (fichaDOM.classList.contains('fichaHover')) {
+        return false;
+    }
+
+
+    if ( fichaDOM.innerHTML!="" ) {
+        seleccionarFicha(fichaDOM)
+
+    } else if(e.path[0].innerHTML===""){
+        ponerFicha(fichaDOM)
+    
+    }
 }
 
-// document.getElementById("container").addEventListener("click", captura_click, false);
+function obtenerEstadoFicha(fichaDOM) {
+    const f = fichaDOM.dataset.fila
+    const c = fichaDOM.dataset.col
+
+    return chess[f][c]
+}
+
+function esMiFicha(fichaDOM) {
+    return obtenerEstadoFicha(fichaDOM).indexOf(turnoActual)===0
+}
+
+function seleccionarFicha(fichaDOM) {
+
+    // Valida si no estoy dando click sobre otra pieza mia
+    if (fichaAnterior && esMiFicha(fichaDOM)) {
+        return false;
+    }
+    else if (fichaAnterior ) {
+        // Validar si estoy es comiendo otra ficha
+        ponerFicha(fichaDOM)
+    }
+
+
+    fichaDOM.classList.add('fichaHover')
+    fichaAnterior = fichaDOM
+
+    // poner efecto de click
+    fichaDOM.classList.add('clicked')
+    setTimeout(function(){
+        fichaDOM.classList.remove('clicked')
+    }, 200)
+}
+
+function actualizarEstado(fichaDOM, fichaAnterior) {
+    const newF = fichaDOM.dataset.fila
+    const newC = fichaDOM.dataset.col
+
+    const oldF = fichaAnterior.dataset.fila
+    const oldC = fichaAnterior.dataset.col
+
+    chess[newF][newC] = chess[oldF][oldC]
+    chess[oldF][oldC] = ""
+}
+
+function ponerFicha(fichaDOM) {
+    fichaDOM.innerHTML = fichaAnterior.innerHTML;
+    fichaAnterior.innerHTML= "";
+    fichaAnterior.classList.remove('fichaHover')
+    
+
+    actualizarEstado(fichaDOM, fichaAnterior)
+
+    fichaAnterior = null
+
+    // if (turnoActual==="W") {
+    //     turnoActual = "B"
+    // } else {
+    //     turnoActual = "W"
+    // }
+
+    // Condicional ternario:
+    turnoActual = (turnoActual==="W") ? "B" : "W"
+}
+
+function validarTurno(fichaDOM) {
+    if (!fichaDOM) {
+        return false
+    }
+
+    const f = fichaDOM.dataset.fila
+    const c = fichaDOM.dataset.col
+
+    if (!f && !c) {
+        return false;
+    }
+
+    // Si tengo ya una ficha seleccionada, permito el turno
+    if (fichaAnterior!==null) {
+        return true
+    }
+
+    const nombreFicha = chess[f][c]
+
+    // ficha: W-Q
+    // turno: W
+    if (nombreFicha.indexOf(turnoActual)===0) {
+        return true
+    }
+
+    return false
+}
+
+// 
 
 // document.querySelectorAll('container').addEventListener("click", captura_click, false);
 
@@ -176,8 +259,4 @@ function chessPieces(){
 // // articleImgs.addEventListener('click', captura_click)
 // 
 
-document.addEventListener("DOMContentLoaded",function(event){
-//FUNCION
-const artd = document.querySelectorAll('td');
-console.log(artd);
-});
+
